9402ed3ae8672f22561a8724a6993ac9c323b04f
lalit-jayanti <94786997+lalit-jayanti@users.noreply.github.com>
2025-08-14 13:37:26 +0000

    Deploying to gh-pages from @ lalit-jayanti/lalit-jayanti.github.io@1ceac4a714caebee78e15f482f531f8deeb50e38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
